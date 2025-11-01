d27a46a082a5b2c019bee042b25a4c48d98c7d4e
praveenvadlamani <pvadl352@gmail.com>
2025-11-01 08:55:54 -0700

    "top - fixed"

--- src/core/game_world.py
+++ src/core/game_world.py
@@ -2405,9 +2405,9 @@ class AnimatedPlayer:
             # Fixed mapping based on actual spritesheet layout:
             idle_config = {
                 'idle_down': (0, 0),   # Column 0, Row 0 = facing down
-                'idle_left': (1, 0),   # Column 1, Row 0 = facing left
+                'idle_left': (3, 0),   # Column 1, Row 0 = facing left
                 'idle_right': (2, 0),  # Column 2, Row 0 = facing right
-                'idle_up': (3, 0),     # Column 3, Row 0 = facing up
+                'idle_up': (1, 0),     # Column 3, Row 0 = facing up
             }
 
             # WALKING SPRITES - Alternating pattern
